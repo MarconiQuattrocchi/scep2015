523819942b17745e5e147e3e81c13adfc3a1c493
francescomarconi <franco.maroni@gmail.com>
2015-09-08 18:06:50 +0200

    "query 2 completed, updateFunction2 missing"

--- spark/spark_taxi.py
+++ spark/spark_taxi.py
@@ -72,13 +72,41 @@ def parseTaxiLogTime(s):
                              int(s[14:16]),
                              int(s[17:19]))
 
-def max_by_dropoff_date((a_start,a_end), (b_start,b_end)):
-    if a_end > b_end:
-#        return ("%s"%a_start,"%s"%a_end)
-        return (a_start,a_end)
+
+def max_by_dropoff_date(a,b):
+    a_start = a[0]
+    a_end = a[1]
+    b_start = b[0]
+    b_end = b[1]
+    if len(a)==3 and len(b)==3:
+        if a_end > b_end:
+            return (a_start,a_end, a[2])
+        else:
+            return (b_start,b_end, b[2])
     else:
-#        return ("%s"%b_start,"%s"%b_end)
-        return (b_start,b_end)
+        if a_end > b_end:
+            return (a_start,a_end)
+        else:
+            return (b_start,b_end)
+
+def max_by_dropoff_and_sum(a,b):
+    a_start = a[0]
+    a_end = a[1]
+    b_start = b[0]
+    b_end = b[1]
+    sum = a[2] + b[2]
+
+    if a_end > b_end:
+        return (a_start, a_end, sum)
+    else:
+        return (b_start, b_end, sum)
+
+
+
+WINDOW_LENGTH_1 = 15
+SLIDING_INTERVAL_1 = 1
+WINDOW_LENGTH_2 = 30
+SLIDING_INTERVAL_2 = 1
 
 
 TAXI_LOG_PATTERN = '^(\S+),(\S+),(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}:\d{2}),(\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}:\d{2}),(\d+),(\d+\.\d+),(\-?\d+\.\d+),(\-?\d+\.\d+),(\-?\d+\.\d+),(\-?\d+\.\d+),(\S+),(\d+\.\d+),(\d+\.\d+),(\d+\.\d+),(\d+\.\d+),(\d+\.\d+),(\d+\.\d+)'
@@ -88,15 +116,17 @@ TOP_LEFT_CELL_CENTER = {'latitude' : 41.474937, 'longitude' : -74.913585}
 LATITUDE_STEP = 0.004491556
 LONGITUDE_STEP = 0.005986
 
-def getLatitudeCell(latitude):
-    floatValue = (TOP_LEFT_CELL_CENTER['latitude'] + LATITUDE_STEP/2 - latitude)/LATITUDE_STEP
+def getLatitudeCell(latitude, resolution):
+    step = LATITUDE_STEP/resolution
+    floatValue = (TOP_LEFT_CELL_CENTER['latitude'] + LATITUDE_STEP/2 - latitude)/step
     if(floatValue>=0):
         return int(floatValue) + 1
     else:
         return -1
 
-def getLongitudeCell(longitude):
-    floatValue = (longitude - TOP_LEFT_CELL_CENTER['longitude'] - LONGITUDE_STEP/2 )/LONGITUDE_STEP
+def getLongitudeCell(longitude, resolution):
+    step = LONGITUDE_STEP/resolution
+    floatValue = (longitude - (TOP_LEFT_CELL_CENTER['longitude'] - LONGITUDE_STEP/2))/step
     if(floatValue>=0):
         return int(floatValue) + 1
     else:
@@ -118,10 +148,16 @@ def parseTaxiLogLine(logline):
     dropoff_datetime        = parseTaxiLogTime(match.group(4)),
     trip_time_in_secs       = int(match.group(5)),
     trip_distance           = float(match.group(6)),
-    pickup_longitude_cell   = getLongitudeCell(float(match.group(7))),
-    pickup_latitude_cell    = getLatitudeCell(float(match.group(8))),
-    dropoff_longitude_cell  = getLongitudeCell(float(match.group(9))),
-    dropoff_latitude_cell   = getLatitudeCell(float(match.group(10))),
+    pickup_longitude_cell   = getLongitudeCell(float(match.group(7)), 1),
+    pickup_latitude_cell    = getLatitudeCell(float(match.group(8)), 1),
+    dropoff_longitude_cell  = getLongitudeCell(float(match.group(9)), 1),
+    dropoff_latitude_cell   = getLatitudeCell(float(match.group(10)), 1),
+# FOR QUERY2
+    pickup_longitude_cell_2  = getLongitudeCell(float(match.group(7)), 2),
+    pickup_latitude_cell_2   = getLatitudeCell(float(match.group(8)), 2),
+    dropoff_longitude_cell_2 = getLongitudeCell(float(match.group(9)), 2),
+    dropoff_latitude_cell_2  = getLatitudeCell(float(match.group(10)), 2),
+
     payment_type            = match.group(11),
     fare_amount             = float(match.group(12)),
     surcharge               = float(match.group(13)),
@@ -161,7 +197,6 @@ def formatResult(resultList):
     timestamps=resultList[0]
     flat_cells = [x for sublists in resultList[1] for x in sublists]
     res = "%s,%s"%(timestamps[0],timestamps[1])
-
     for i in flat_cells:
         res="%s,%s"%(res,i)
     return res
@@ -191,70 +226,75 @@ def createContext(host, port, outputPath1, outputPath2):
                                  dropoff_datetime   = trip.dropoff_datetime,
                                  start_cell_id      = "%s.%s"%(trip.pickup_latitude_cell, trip.pickup_longitude_cell),
                                  end_cell_id        = "%s.%s"%(trip.dropoff_latitude_cell, trip.dropoff_longitude_cell),
+                                 #QUERY2
+                                 start_cell_id_2      = "%s.%s"%(trip.pickup_latitude_cell_2, trip.pickup_longitude_cell_2),
+                                 end_cell_id_2        = "%s.%s"%(trip.dropoff_latitude_cell_2, trip.dropoff_longitude_cell_2),
                                  fare_amount        = trip.fare_amount,
                                  tip_amount         = trip.tip_amount,
                                  medallion          = trip.medallion))
 
 
 
-
-
-    tripsByCellsAndWindowsForCount = queryFormat.map(lambda trip: ((trip.start_cell_id, trip.end_cell_id), (trip.pickup_datetime, trip.dropoff_datetime))).window(10,1).cache()
+    firstWindowedData = queryFormat.window(WINDOW_LENGTH_1, SLIDING_INTERVAL_1).cache()
+    secondWindowedData = queryFormat.window(WINDOW_LENGTH_2, SLIDING_INTERVAL_2).cache()
 
 
 #    tripsByCellsAndWindows.checkpoint(120)
 
+    ###QUERY1
+    tripsByCellsAndWindowsForCount = firstWindowedData.map(lambda trip: ((trip.start_cell_id, trip.end_cell_id), (trip.pickup_datetime, trip.dropoff_datetime)))
+
     tripCountsByCells = tripsByCellsAndWindowsForCount.transform(lambda rdd: rdd.map(lambda (k,v): (k, 1)).reduceByKey(lambda x, y: x + y))
 
-    ####QUERY2
-    tripsByCellsAndWindowsForProfit = queryFormat.map(lambda trip: (trip.start_cell_id, trip.fare_amount + trip.tip_amount)).window(5,3).cache()
-
-    tripsByMedallionsAndWindowsForEmptyTaxies = queryFormat.map(lambda trip: (trip.medallion, (trip.end_cell_id, trip.dropoff_datetime))).window(5,3).cache()
-
-    profitByCell = tripsByCellsAndWindowsForProfit.transform(lambda rdd: rdd.groupByKey().map(lambda (k,v): (k,median(v))).sortByKey(False))
-
-    profitByCell.pprint()
-
-    lastTripsByMedallion = tripsByMedallionsAndWindowsForEmptyTaxies.transform(lambda rdd: rdd.reduceByKey(lambda a,b: max_by_dropoff_date(a,b)).sortByKey(False))
-#   lastTripsByMedallion.pprint()
-    countEmptyTaxiesByCells = lastTripsByMedallion.transform(lambda rdd: rdd.map(lambda (k,v):(v[0],1)).reduceByKey(lambda x,y:x+y).sortBy(lambda (k,v):v, ascending=False))
-#    countEmptyTaxiesByCells.pprint()
-
-    profitabilityByCell = profitByCell.join(countEmptyTaxiesByCells).map(lambda x:('CULO',x))
-    profitabilityByCell.pprint()
 
     #TAKING INTO ACCOUNT THE LAST DROPOFF TIME
     tripMaxDropoffBycell = tripsByCellsAndWindowsForCount.transform(lambda rdd: rdd.reduceByKey(lambda a,b: max_by_dropoff_date(a,b)).map(lambda (k,v): (k,("%s"%v[0],"%s"%v[1]))))
 
 
-#PRINT
     tripCountsByCells.pprint()
     tripMaxDropoffBycell.pprint()
-
-
-#    tripMaxDropoffBycell.checkpoint(120)
-
-
-
-#    tripCountsByCells.checkpoint(120)
-    #
-    # sortedTripCounts = tripCountsByCells.map(lambda (trip, count):(count,trip)).transform(lambda rdd: rdd.sortByKey(False).map(lambda (trip, count):(count,trip)))
-    #
-    # sortedTripCounts.pprint()
-    #
-    # completeSet = sortedTripCounts.join(tripMaxDropoffBycell)
-    #
 
     completeSet = tripCountsByCells.join(tripMaxDropoffBycell).map(lambda (k, v): (v[0],(k, v))).transform(lambda rdd: rdd.sortBy(lambda (count, v): (count, v[0]) ,ascending=False).map(lambda (count, tuple):tuple))
 
 #PRINT
-    completeSet.pprint()
+#    completeSet.map(lambda x: ('COMPLETE_SET',x)).pprint()
     #EXPECTED SOMETHING LIKE: (('169.152', '168.152'), ('2015-07-17 12:00:55', '2015-07-17 12:02:55'))
 
-#    list of the top three words per window with their position as key (ex. [(1,'word1'),(2,'word2')])
-    topTenTripsWithIndex = completeSet.transform(lambda rdd: rdd.zipWithIndex().filter(lambda (k,v): v<10).map(lambda (k,v):k))
+#   list of the top ten Routes per window
+    topTenTrips = completeSet.transform(lambda rdd: rdd.zipWithIndex().filter(lambda (k,v): v<10).map(lambda (k,v):k))
 
-    topTenTripsWithIndex.pprint()
+#    topTenTrips.map(lambda x: ('WHAEE',x)).pprint()
+
+
+
+    ####QUERY2
+    tripsByCellsAndWindowsForProfit = firstWindowedData.map(lambda trip: (trip.start_cell_id_2, (trip.pickup_datetime, trip.dropoff_datetime, trip.fare_amount + trip.tip_amount)))
+
+    tripsByMedallionsAndWindowsForEmptyTaxies = secondWindowedData.map(lambda trip: (trip.medallion, (trip.pickup_datetime, trip.dropoff_datetime, trip.end_cell_id_2))).cache()
+
+    #profitByCell = tripsByCellsAndWindowsForProfit.transform(lambda rdd: rdd.reduceByKey(lambda a,b: max_by_dropoff_date(a,b)).map(lambda (k,v): (k,(median(v[2]),v[0],v[1]))))
+
+    medianProfitByCell = tripsByCellsAndWindowsForProfit.transform(lambda rdd: rdd.map(lambda (k,v): (k,v[2])).groupByKey().map(lambda (k,v): (k,median(v))))
+    lastProfitEventByCell = tripsByCellsAndWindowsForProfit.transform(lambda rdd: rdd.map(lambda (k,v): (k,(v[0],v[1]))).reduceByKey(lambda a,b: max_by_dropoff_date(a,b)).map(lambda (k,v): (k,("%s"%v[0],"%s"%v[1]))))
+
+    profitCompleteByCell = lastProfitEventByCell.join(medianProfitByCell)
+
+    profitCompleteByCell.map(lambda x: ('PROFIT_BY_CELL',x)).pprint()
+
+    lastTripsByMedallion = tripsByMedallionsAndWindowsForEmptyTaxies.transform(lambda rdd: rdd.reduceByKey(lambda a,b: max_by_dropoff_date(a,b)).sortByKey(False))
+#   lastTripsByMedallion.pprint()
+    countEmptyTaxiesByCells = lastTripsByMedallion.transform(lambda rdd: rdd.map(lambda (k,v):(v[2],(v[0],v[1],1))).reduceByKey(lambda a,b: max_by_dropoff_and_sum(a,b)).map(lambda (k,v):(k,("%s"%v[0],"%s"%v[1],v[2]))).sortBy(lambda (k,v):v[2], ascending=False))
+    countEmptyTaxiesByCells.map(lambda x: ('EMPTY',x)).pprint()
+
+    profitabilityByCell = profitCompleteByCell.join(countEmptyTaxiesByCells).transform(lambda rdd: rdd.map(lambda (cell,(((pickup_prof,dropoff_prof),prof),(pickup_empty,dropoff_empty,empty_count))): (cell, (empty_count, prof, prof/empty_count, pickup_empty, dropoff_empty, pickup_prof, dropoff_prof))).sortBy(lambda x: x[3], ascending=False))
+#    profitabilityByCell = profitCompleteByCell.join(countEmptyTaxiesByCells)
+    profitabilityByCell.map(lambda x: ('PROFITABILITY',x)).pprint()
+
+    topTenProfitableAreas = profitabilityByCell.transform(lambda rdd: rdd.zipWithIndex().filter(lambda (k,v): v<10).map(lambda (k,v):k))
+
+#    tripMaxDropoffBycell.checkpoint(120)
+
+
 
     def updateFunc(new_values, last_list):
         if last_list is None:
@@ -278,10 +318,50 @@ def createContext(host, port, outputPath1, outputPath2):
         diff = [j for i, j in zip(paddedOldRanking, new_ranking) if i != j]
         if len(diff)>0:
             print("new elements: %s (new ranking: %s)" % (diff, new_ranking))
-            new_datez = [v[1] for (k,v) in new_values[0] if k in diff]
+            new_datez = [(v[1][1],v[1][0]) for (k,v) in new_values[0] if k in diff]
             print("NEW_DATEZ: %s"%new_datez)
             new_dates = max(new_datez)
-            new_result = [new_dates, new_ranking]
+            new_result = [(new_dates[1],new_dates[0]), new_ranking]
+            #write the new ranking on file outputPath2 only when there is a change in any of the the top N positions
+            new_string=formatResult(new_result)
+            with open(outputPath2, 'a') as f:
+                f.write(new_string + "\n\n")
+        else:
+            new_result=last_list
+        # write the comparison between old and new status on file outputPath1 every time updateFunc is invoked
+        with open(outputPath1, 'a') as f:
+            f.write(counts + "\n")
+        return new_result
+
+
+
+#TODO COMPLETARE PER QUERY 2
+    def updateFunc2(new_values, last_list):
+        if last_list is None:
+            last_ranking = []
+        else:
+            last_ranking = last_list[2:] #ignore pickup_datetime and dropoff_datetime
+
+#        print("NEW-VALUES: %s" % new_values)
+
+        if len(new_values) > 0:
+            new_ranking = [k for (k,v) in new_values[0]]
+        else:
+            new_ranking = []
+
+        counts = "OLD: %s --> NEW: %s" % (last_ranking, new_ranking)
+
+        new = "%s" % new_values
+        print("updateFunc: "+counts)
+        #
+        paddedOldRanking =  [i for i,j in list(itertools.izip_longest(last_ranking, range(10)))]
+        diff = [j for i, j in zip(paddedOldRanking, new_ranking) if i != j]
+        if len(diff)>0:
+            print("new elements: %s (new ranking: %s)" % (diff, new_ranking))
+            new_datez = [(v[1][1],v[1][0]) for (k,v) in new_values[0] if k in diff]
+            print("NEW_DATEZ: %s"%new_datez)
+            new_dates = max(new_datez)
+            new_result = [(new_dates[1],new_dates[0]), new_ranking]
             #write the new ranking on file outputPath2 only when there is a change in any of the the top N positions
             new_string=formatResult(new_result)
             with open(outputPath2, 'a') as f:
@@ -296,10 +376,13 @@ def createContext(host, port, outputPath1, outputPath2):
 
 
 
-    rankingTripCount = topTenTripsWithIndex.transform(lambda rdd:rdd.map(lambda x:('ranking',x)).groupByKey().map(lambda x : (x[0], list(x[1]))))
 
+    rankingTripCount = topTenTrips.transform(lambda rdd:rdd.map(lambda x:('ranking',x)).groupByKey().map(lambda x : (x[0], list(x[1]))))
+
+    rankingProfitability = topTenProfitableAreas.transform(lambda rdd:rdd.map(lambda x:('ranking2',x)).groupByKey().map(lambda x : (x[0], list(x[1]))))
 #u    rankingTripCount = sortedTripCounts.transform(lambda rdd:rdd.map(lambda (k,v):('ranking',v)).groupByKey().map(lambda x : (x[0], list(x[1]))))
     rankingTripCount.pprint()
+    rankingProfitability.pprint()
     #invoke updateFunc for each time window
     status = rankingTripCount.updateStateByKey(updateFunc)
     status.pprint()
